bdc5dde3aa2f76c7543097dc5e11107b09b9f027
jutn-dev <51607369+jutn-dev@users.noreply.github.com>
2025-05-22 17:03:55 +0300

    done some testng

--- lua/vehicle/extensions/auto/gears.lua
+++ lua/vehicle/extensions/auto/gears.lua
@@ -7,6 +7,7 @@ M.reverse_gear = 15
 M.gear_timer = 0
 -- the time it takes to change a gear after the action
 M.gear_swaptime = 0.3
+M.joystick_mode = true
 function onUpdate()
   if M.gear_timer > 0 then
   	if os.clock() > M.gear_timer then
@@ -16,7 +17,25 @@ function onUpdate()
   end
 end
 
-function change_gear_bit(gear)
+--value is used for joystck mode
+-- in joystck mode 0 and 2 are the same
+-- in joystck mode 1 and 3 are the same
+function change_gear_bit(gear, value)
+  print("G "..gear)
+  print("V "..value)
+
+  if joystick_mode then
+  	if gear == 0 || gear == 2 then
+	  if value > 0 then
+		M.gear_input = swap_bit(M.gear_input, 0)
+	  else
+
+	  end
+	end
+	if gear == 1 || gear == 3 then
+  	
+	end	
+  end
   M.gear_input = swap_bit(M.gear_input, gear)
   M.gear_timer = tonumber(os.clock() + M.gear_swaptime)
 end
@@ -49,6 +68,14 @@ function help()
 	print("---- gears help ----")
 	print("Debug mode:"..M.debug)
 	print("gears.help() prints this message \ngears.change_gear() changes the gear to value stored in M.gear_input \ngears.print_current_gear() prints the value of M.gear_input \n M.debug can be used to set on debug mode \n M.reverse_gear can be used to change which input number is reverse gear")
+end
+
+functon reset()
+	M.gear_input = 0
+	M.debug = true
+	M.reverse_gear = 15
+	M.gear_timer = 0
+	M.gear_swaptime = 0.3
 end
 
 M.help = help
